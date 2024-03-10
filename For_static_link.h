b37c0e1405583e15969e828c1bfbb8b25ef1ad97
GlebNevolin <dedalnev@gmail.com>
2024-03-10 21:05:39 +0500

    Initial commit

---
+++ For_static_link.h
@@ -0,0 +1,3 @@
+#pragma once
+extern "C" void __stdcall NameSurname(char* p);
+extern "C" int __stdcall GroupNumber();
---
+++ dllmain.cpp
@@ -0,0 +1,32 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+#include "For_static_link.h"
+#include <iostream>
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+extern "C" void __stdcall NameSurname(char* p)
+{
+    char a[] = "Nevolin Gleb Olegovich";
+    strcpy(p, a);
+}
+
+extern "C" int __stdcall GroupNumber()
+{
+    int i = 591220;
+    return i;
+}
